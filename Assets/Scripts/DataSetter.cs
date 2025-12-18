e18a8306bb442e68a0e3541c01483917491023eb
uwatcha <alterego102u3v3@gmail.com>
2025-12-18 12:27:20 +0900

    不要なコード削除

--- Assets/Scripts/DataSetter.cs
+++ Assets/Scripts/DataSetter.cs
@@ -43,31 +43,5 @@ public class DataSetter : MonoBehaviour
         }
 
         techCountChart.RefreshChart();
-        return;
-        var techCounts = datas
-            .GroupBy(d => d.techID)
-            .OrderByDescending(g => g.Key)
-            .ToDictionary(g => TechIdNameDict.Dict[g.Key], g => g.Count());
-        Logger.LogElements("techCounts", techCounts.Select(kv => $"{kv.Key}: {kv.Value}").ToList());
-        
-        // チャートをクリア
-        techCountChart.RemoveData();
-
-        // シリーズがなければ追加
-        if (techCountChart.series.Count == 0)
-        {
-            techCountChart.AddSerie<Bar>("Tech");
-        }
-
-        // データを追加
-        foreach (var tech in techCounts)
-        {
-            Logger.Log($"Adding to chart: {tech.Key} with count {tech.Value}");
-            techCountChart.AddYAxisData(tech.Key);
-            techCountChart.AddData(0, tech.Value);
-        }
-
-        // チャートを更新
-        techCountChart.RefreshChart();
     }
 }
